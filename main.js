739433165f56b1aa064c657cbd63aeba6c918a33
Davis Allen <davis.b.allen@gmail.com>
2019-09-23 15:38:56 -0400

    Hook up ajax call to weather api

--- main.js
+++ main.js
@@ -18,11 +18,31 @@ STEPS
 
 */
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=New+York&units=imperial&appid=f789ebd868b620fa978ed20f8e430f6e')
-  .then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    console.log(response);
-    document.querySelector("#test").append(response.name);
+const appid = '7486b3fcce2d7615695d34c792cdef14';
+
+function fetchWeatherAndUpdateUi(usZipCode) {
+  let baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  let fullEndpoint = `${baseUrl}?zip=${usZipCode},us&appid=${appid}&units=imperial`;
+  fetch(fullEndpoint)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelector('#btn').addEventListener('click', () => {
+    let zip = document.querySelector('#zipcode').value;
+    fetchWeatherAndUpdateUi(zip);
   });
+});
+// fetch('http://api.openweathermap.org/data/2.5/weather?q=New+York&units=imperial&appid=f789ebd868b620fa978ed20f8e430f6e')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((response) => {
+//     console.log(response);
+//     document.querySelector("#test").append(response.name);
+//   });
